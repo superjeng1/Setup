299edf9f0227db46322d1ad09c8f704b1a807598
Willy Hsiao <33928963+superjeng1@users.noreply.github.com>
2018-08-26 16:31:37 +0800

    Update IKEv2-setup.sh

--- IKEv2-setup.sh
+++ IKEv2-setup.sh
@@ -753,6 +753,7 @@ read -s -p "Setup is almost finished. Press [Return] to Install BBR now and Rebo
 echo
 read -n 1 -s -r -p "Please RE-RUN this script after reboot. It will automaticly finish the installation. Press any key to continue..."
 echo
+echo
 touch ~/raninstallbbr
 wget -qO 'BBR.sh' 'https://moeclub.org/attachment/LinuxShell/BBR.sh'
 bash BBR.sh -f
